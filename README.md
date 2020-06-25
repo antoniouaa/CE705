33d635ab844b76b206df44c9375560c52ff149e2
antoniouaa <antoniouaa@hotmail.com>
2020-06-25 20:19:17 +0100

    intro to python assignment now pushed to github

---
+++ README.md
@@ -0,0 +1,9 @@
+# CE705 Introduction to Programming in Python Assignment
+
+Have to write a classifier to group wines into a number of categories, based on similarity of their attributes.
+
+Data is provided in the form of 178 wines are in the form of 1x13 vectors.
+
+Algorithm to implement is k-means clustering.
+
+An implementation using sklearn is also included for comparison.
---
+++ kmeans_wine.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+
+"""
+a dict to collect all the wines associated with
+a cluster
+"""
+class autorefresh_list(dict):
+    def __missing__(self, key):
+        value = self[key] = []
+        return value
+    
+    def __add__(self, x):
+        if not self and isinstance(x, Number):
+            return x
+        raise ValueError
+        
+    def __sub__(self, x):
+        if not self and isinstance(x, Number):
+            return -1 * x
+        raise ValueError
+
+"""
+grabs the data from the file and puts them in 
+numpy arrays
+"""
+def get_matrix():
+    from assignment import load_from_csv
+    M = load_from_csv("Data.csv")
+    values_array = np.array([x for x in M])
+    labels_array = [f"wine{i}" for i in range(values_array.shape[0])]
+    return values_array, labels_array
+
+"""
+picks out wines which are associated with a cluster and puts them in the autorefresh list
+"""
+def find_wine_clusters(labels_array, cluster_labels):
+    cluster_to_words = autorefresh_list()
+    for i, wine in enumerate(cluster_labels):
+        cluster_to_words[wine].append(labels_array[i])
+    return cluster_to_words
+
+from timeit import default_timer
+
+start = default_timer()
+
+M, labels_array = get_matrix()
+for n_clusters in range(2, 7):
+    """
+    Use a k-means model with random initial
+    cluster centroids from the data
+    """
+    kmeans_model = KMeans(init="random",
+                      n_clusters=n_clusters, 
+                      n_init=10)
+    kmeans_model.fit(M)
+    
+    cluster_labels = kmeans_model.labels_
+    cluster_inertia = kmeans_model.inertia_
+    cluster_to_words = find_wine_clusters(labels_array, cluster_labels)
+
+    print(f"Number of clusters: {n_clusters}")
+    for wine in sorted(cluster_to_words):
+        print(f"Cluster {wine+1} has {len(cluster_to_words[wine])} entities")
+    print()
+
+end = default_timer()
+print(f"Time taken: {end-start}")
